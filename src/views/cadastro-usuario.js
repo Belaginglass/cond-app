f98e9d400858ae869fbf49599cf0e09775d5bbcf
Bernardo Duarte <bernardoduarte.mg@gmail.com>
2025-01-21 10:08:40 -0300

    listagem e cadastro de usuários

---
+++ src/views/cadastro-usuario.js
@@ -0,0 +1,159 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+import Stack from "@mui/material/Stack";
+
+import Card from "../components/card";
+import FormGroup from "../components/form-group";
+
+ import { mensagemSucesso, mensagemErro } from "../components/toastr";
+
+// import "../custom.css";
+
+import axios from "axios";
+import { BASE_URL } from "../config/axios";
+
+function CadastroUsuario() {
+  const { idParam } = useParams();
+
+  const navigate = useNavigate();
+
+  const baseURL = `${BASE_URL}/usuarios`;
+
+  const [id, setId] = useState("");
+  const [cpf, setCpf] = useState("");
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
+  const [dados, setDados] = React.useState([]);
+
+  function inicializar() {
+    if (idParam == null) {
+      setId("");
+      setCpf("");
+      setEmail("");
+      setSenha("");
+    } else {
+      setId(dados.id);
+      setCpf(dados.cpf);
+      setEmail(dados.email);
+      setSenha(dados.senha);
+    }
+  }
+
+  async function salvar() {
+    let data = { id, cpf, email, senha };
+    data = JSON.stringify(data);
+    if (idParam == null) {
+      await axios
+        .post(baseURL, data, {
+          headers: { "Content-Type": "application/json" },
+        })
+        .then(function (response) {
+          mensagemSucesso(`Usuário ${cpf} cadastrado com sucesso!`);
+          navigate(`/listagem-usuarios`);
+        })
+        .catch(function (error) {
+          mensagemErro(error.response.data);
+        });
+    } else {
+      await axios
+        .put(`${baseURL}/${idParam}`, data, {
+          headers: { "Content-Type": "application/json" },
+        })
+        .then(function (response) {
+          mensagemSucesso(`Usuário ${cpf} alterado com sucesso!`);
+          navigate(`/listagem-usuarios`);
+        })
+        .catch(function (error) {
+          mensagemErro(error.response.data);
+        });
+    }
+  }
+
+  async function buscar() {
+    await axios.get(`${baseURL}/${idParam}`).then((response) => {
+      setDados(response.data);
+    });
+    setId(dados.id);
+    setCpf(dados.cpf);
+    setEmail(dados.email);
+    setSenha(dados.senha);
+  }
+
+  const [dadosUsuarios, setDadosUsuarios] = React.useState(null);
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/usuarios`).then((response) => {
+      setDadosUsuarios(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    buscar(); // eslint-disable-next-line
+  }, [id]);
+
+  if (!dados) return null;
+  if (!dadosUsuarios) return null;
+
+  return (
+    <div className="container">
+      <Card title="Cadastro de Usuário">
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="bs-component">
+              <FormGroup label="CPF: *" htmlFor="inputCpf">
+                <input
+                  type="text"
+                  id="inputCpf"
+                  value={cpf}
+                  className="form-control"
+                  name="cpf"
+                  onChange={(e) => setCpf(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup label="E-mail: *" htmlFor="inputEmail">
+                <input
+                  type="text"
+                  id="inputEmail"
+                  value={email}
+                  className="form-control"
+                  name="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup label="Senha: *" htmlFor="inputSenha">
+                <input
+                  type="text"
+                  id="inputSenha"
+                  value={senha}
+                  className="form-control"
+                  name="senha"
+                  onChange={(e) => setSenha(e.target.value)}
+                />
+              </FormGroup>
+              <Stack spacing={1} padding={1} direction="row">
+                <button
+                  onClick={salvar}
+                  type="button"
+                  className="btn btn-success"
+                >
+                  Salvar
+                </button>
+                <button
+                  onClick={inicializar}
+                  type="button"
+                  className="btn btn-danger"
+                >
+                  Cancelar
+                </button>
+              </Stack>
+            </div>
+          </div>
+        </div>
+      </Card>
+    </div>
+  );
+}
+
+export default CadastroUsuario;
--- src/views/listagem-usuarios.js
+++ src/views/listagem-usuarios.js
@@ -1,81 +1,99 @@
-import React from 'react';
+import React from "react";
 
-import Card from '../components/card';
+import Card from "../components/card";
 
-import Stack from '@mui/material/Stack';
-import { IconButton } from '@mui/material';
-import DeleteIcon from '@mui/icons-material/Delete';
-import EditIcon from '@mui/icons-material/Edit';
+import { useNavigate } from "react-router-dom";
 
-import axios from 'axios';
-import { BASE_URL } from '../config/axios';
+import Stack from "@mui/material/Stack";
+import { IconButton } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+
+import axios from "axios";
+import { BASE_URL } from "../config/axios";
 
 const baseURL = `${BASE_URL}/usuarios`;
 
 function ListagemUsuarios() {
-    const [dados, setDados] = React.useState(null);
+  const navigate = useNavigate();
 
-    React.useEffect(() => {
-        axios.get(baseURL).then((response) => {
-          setDados(response.data);
-        });
-      }, []);
-    
-      if (!dados) return null;
-    
-    return (
-        <div className='container'>
-        <Card title='Listagem de Usuários'>
-            <div className='row'>
-            <div className='col-lg-12'>
-                <div className='bs-component'>
-                { <button
-                    type='button'
-                    className='btn btn-warning'
-                    // onClick={() => cadastrar()}
+  const cadastrar = () => {
+    navigate(`/cadastro-usuario`);
+  };
+
+  const editar = (id) => {
+    navigate(`/cadastro-usuario/${id}`);
+  };
+
+  const [dados, setDados] = React.useState(null);
+
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setDados(response.data);
+    });
+  }, []);
+
+  if (!dados) return null;
+
+  return (
+    <div className="container">
+      <Card title="Listagem de Usuários">
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="bs-component">
+              {
+                <button
+                  type="button"
+                  className="btn btn-warning"
+                  onClick={() => cadastrar()}
                 >
-                    Novo Usuário
-                </button> }
-                <table className='table table-hover'>
-                    <thead>
-                    <tr>
-                        <th scope='col'>Email</th>
-                        <th scope='col'>CPF</th>
-                        <th scope='col'>Senha</th>
-                        <th scope='col'>Ações</th>
-                    </tr>
-                    </thead>
-                    { <tbody>
+                  Novo Usuário
+                </button>
+              }
+              <table className="table table-hover">
+                <thead>
+                  <tr>
+                    <th scope="col">Email</th>
+                    <th scope="col">CPF</th>
+                    <th scope="col">Senha</th>
+                    <th scope="col">Ações</th>
+                  </tr>
+                </thead>
+                {
+                  <tbody>
                     {dados.map((dado) => (
-                        <tr key={dado.id}>
+                      <tr key={dado.id}>
                         <td>{dado.email}</td>
                         <td>{dado.cpf}</td>
                         <td>{dado.senha}</td>
                         <td>
-                            { <Stack spacing={1} padding={0} direction='row'>
-                            <IconButton
-                                aria-label='edit'
-                                //onClick={() => editar(dado.id)}
-                            >
+                          {
+                            <Stack spacing={1} padding={0} direction="row">
+                              <IconButton
+                                aria-label="edit"
+                                onClick={() => editar(dado.id)}
+                              >
                                 <EditIcon />
-                            </IconButton>
-                            <IconButton
-                                aria-label='delete'
-                               // onClick={() => excluir(dado.id)}
-                            >
+                              </IconButton>
+                              <IconButton
+                                aria-label="delete"
+                                // onClick={() => excluir(dado.id)}
+                              >
                                 <DeleteIcon />
-                            </IconButton>
-                            </Stack> }
+                              </IconButton>
+                            </Stack>
+                          }
                         </td>
-                        </tr>
+                      </tr>
                     ))}
-                    </tbody> }
-                </table>{' '}
-                </div>
+                  </tbody>
+                }
+              </table>{" "}
             </div>
-            </div>
-        </Card>
+          </div>
         </div>
-    );
+      </Card>
+    </div>
+  );
 }
 export default ListagemUsuarios;
